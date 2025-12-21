06e1b94e8f5476aac6abc039f5e27550f04cf9f5
Hajeera <hajeerajobs@gmail.com>
2025-12-21 20:24:33 +0530

    Fix movement and object handling in Around3.py
    Refactor movement logic to ensure consistent execution.

--- Around3.py
+++ Around3.py
@@ -1,19 +1,15 @@
 #This solution works for Around 1 & Around 2 too
 put()
-​
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-​
 while True:
     if wall_in_front():
         turn_left()
     if right_is_clear():
         turn_right()
-    if front_is_clear():
-        move()
+    move()
     if object_here():
         take()
-        done()
-    
+        done()    
