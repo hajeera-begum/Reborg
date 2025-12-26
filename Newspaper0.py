9007246b5522f8ff6c2cdc11bba45a7f629861fb
Hajeera <hajeerajobs@gmail.com>
2025-12-27 01:04:38 +0530

    Add movement functions and logic for robot navigation
    Implement movement functions for the robot.

---
+++ Newspaper0.py
@@ -0,0 +1,34 @@
+def turn_right():
+    for i in range(3):
+        turn_left()
+def move_3steps():
+    for i in range(2):
+        move()
+   
+take()
+for i in range(3):
+    turn_left()
+    move()
+    turn_right()
+    move_3steps()
+put()
+
+turn_left()
+turn_left()
+move()
+move()
+turn_left()
+move()
+turn_right()
+move()
+move()
+turn_left()
+move()
+turn_right()
+move()
+move()
+turn_left()
+move()
+
+
+
