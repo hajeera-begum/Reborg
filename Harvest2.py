cbc2d772297c61de585b9590c15e19fc47d2f8d6
Hajeera <hajeerajobs@gmail.com>
2025-12-27 00:00:31 +0530

    Implement harvesting and movement functions

---
+++ Harvest2.py
@@ -0,0 +1,30 @@
+def turn_right():
+    for i in range(3):
+        turn_left()
+    
+turn_left()
+move()
+move()
+turn_right()
+move()
+
+def collect():
+    while object_here():
+        take()
+def harvest(num):
+    for i in range(num):
+        move()
+        collect()
+def step_up(direction):
+    direction()
+    move()
+    direction()
+    collect()
+harvest(6)
+for i in range(2):
+    step_up(turn_left)
+    harvest(5)
+    step_up(turn_right)
+    harvest(5)
+step_up(turn_left)
+harvest(5)
